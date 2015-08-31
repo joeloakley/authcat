541fd6d66568168490eaea6e96dd62a2605b839b
joel <joeloakley@gmail.com>
2015-08-31 14:56:03 -0700

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, length: { minimum: 5}
 
 end
